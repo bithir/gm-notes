f3dc38c25ecd7e7d631376f819248e47179fd65e
Peter Norell <bithir@gmail.com>
2024-05-23 20:50:16 +0100

    Added API support and keybind for opening a GMNote Window

--- gm-notes.js
+++ gm-notes.js
@@ -1,217 +1,227 @@
 import { sendDevMessage } from './devmessage.js';
 class GMNote extends FormApplication {
+	static alwaysHideLabelForSystem = ["dnd5e"];
 
-    static alwaysHideLabelForSystem = [ 'dnd5e'];
+	static get shouldHideLabel() {
+		return game.settings.get("gm-notes", "hideLabel") || GMNote.alwaysHideLabelForSystem.includes(game.system.id);
+	}
 
-    static get shouldHideLabel() {
-        return  game.settings.get('gm-notes', 'hideLabel') || 
-            GMNote.alwaysHideLabelForSystem.includes(game.system.id);
-    }
-    constructor(object, options) {
-        super(object, options);
-        this.object.apps[this.appId] = this;
-    }
+	static showGMNoteWindow() {
+		const win = ui.activeWindow;        
+		if (!win || win._state != Application.RENDER_STATES.RENDERED || !win.object) {
+			return;
+		}
+		new gmnote.GMNote(ui.activeWindow.object, { submitOnClose: true, closeOnSubmit: false, submitOnUnfocus: true }).render(true);
+	}
 
-    get showExtraButtons() {
-        return (game.dnd5e && this.object.constructor.name !== 'RollTable' || this.object.constructor.name === "JournalEntry");
-    }
+	constructor(object, options) {
+		super(object, options);
+		this.object.apps[this.appId] = this;
+	}
 
-    static get defaultOptions() {
-        const options = super.defaultOptions;
-        options.template = "modules/gm-notes/templates.html";
-        options.width = '600';
-        options.height = '700';
-        options.classes = ['gm-notes', 'sheet'];
-        options.title = game.i18n.localize('GMNote.label');
-        options.resizable = true;
-        options.editable = true;
-        return options;
-    }
+	get showExtraButtons() {
+		return (game.dnd5e && this.object.constructor.name !== "RollTable") || this.object.constructor.name === "JournalEntry";
+	}
 
-    async getData() {
-        const data = super.getData();
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.template = "modules/gm-notes/templates.html";
+		options.width = "600";
+		options.height = "700";
+		options.classes = ["gm-notes", "sheet"];
+		options.title = game.i18n.localize("GMNote.label");
+		options.resizable = true;
+		options.editable = true;
+		return options;
+	}
 
-    
-        let page = null;
-        if(this.object.constructor.name === 'JournalEntry') 
-        {
-            // Current page is on another event loop - wait for 50 millis solves it in majority of circumstances
-            await this.sleep(100);
-            page = this.getCurrentPage();
-        }
+	async getData() {
+		const data = super.getData();
 
+		let page = null;
+		if (this.object.constructor.name === "JournalEntry") {
+			// Current page is on another event loop - wait for 50 millis solves it in majority of circumstances
+			await this.sleep(100);
+			page = this.getCurrentPage();
+		}
 
-        data.journalNotes = await TextEditor.enrichHTML(this.object.getFlag('gm-notes', 'notes'), { async:true});
-        data.flags = this.object.flags;
-        data.owner = game.user.id;
-        data.isGM = game.user.isGM;
-        data.showExtraButtons = this.showExtraButtons && page != null;
+		data.journalNotes = await TextEditor.enrichHTML(this.object.getFlag("gm-notes", "notes"), { async: true });
+		data.flags = this.object.flags;
+		data.owner = game.user.id;
+		data.isGM = game.user.isGM;
+		data.showExtraButtons = this.showExtraButtons && page != null;
 
-        return data;
-    }
+		return data;
+	}
 
-    getCurrentPage()
-    {
-        if(this.object.constructor.name !== 'JournalEntry') { 
-            return null;
-        }
-        // Find current page
-        let pageIdentifier = $(this.object.sheet.pagesInView[0]).data("pageId");
+	getCurrentPage() {
+		if (this.object.constructor.name !== "JournalEntry") {
+			return null;
+		}
+		// Find current page
+		let pageIdentifier = $(this.object.sheet.pagesInView[0]).data("pageId");
 
-        if(pageIdentifier) {
-            return this.object.pages.get(pageIdentifier);
-        }
-        return null;
-    }
+		if (pageIdentifier) {
+			return this.object.pages.get(pageIdentifier);
+		}
+		return null;
+	}
 
-    async sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+	async sleep(ms) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
 
-    activateListeners(html) {
-        super.activateListeners(html);
+	activateListeners(html) {
+		super.activateListeners(html);
 
-        html.find('.moveToNote').click(ev => this._moveToNotes());
-        html.find('.moveToDescription').click(ev => this._moveToDescription());
+		html.find(".moveToNote").click((ev) => this._moveToNotes());
+		html.find(".moveToDescription").click((ev) => this._moveToDescription());
 
-        // Update Title
-        const elem = html[0];
-        elem.closest('.window-app').querySelector('header > .window-title').textContent = game.i18n.format('GMNote.title', { document: this.object.name });
-    }
-    
-    async _updateObject(event, formData) {
-        if (jQuery.isEmptyObject(formData) ) {
-            return;
-        }
-        if (game.user.isGM) {
-            await this.object.setFlag('gm-notes', 'notes', formData["flags.gm-notes.notes"]);
-            // this.render();
-        } else {
-            ui.notifications.error("You have to be GM to edit GM Notes.");
-        }
-    }
+		// Update Title
+		const elem = html[0];
+		elem.closest(".window-app").querySelector("header > .window-title").textContent = game.i18n.format("GMNote.title", { document: this.object.name });
+	}
 
-    static _attachHeaderButton(app, buttons) {
-        // Ignore JournalTextPageSheets
-        if(app instanceof JournalTextPageSheet) return;
+	async _updateObject(event, formData) {
+		if (jQuery.isEmptyObject(formData)) {
+			return;
+		}
+		if (game.user.isGM) {
+			await this.object.setFlag("gm-notes", "notes", formData["flags.gm-notes.notes"]);
+			// this.render();
+		} else {
+			ui.notifications.error("You have to be GM to edit GM Notes.");
+		}
+	}
 
-        // If user is not GM - don't do anything
-        if (!game.user.isGM) return;
+	static _attachHeaderButton(app, buttons) {
+		// Ignore JournalTextPageSheets
+		if (app instanceof JournalTextPageSheet) return;
 
-        // If app has document
-        if (!(app.document instanceof foundry.abstract.Document)) return;
-        
-        buttons.unshift({
-            // If hide label is true, don't show label
-            label: game.i18n.localize('GMNote.label'),
-            tooltip: game.i18n.localize('GMNote.label'),
-            class: 'open-gm-note',
-            get icon() {
-                // Get GM Notes
-                const notes = app.document.getFlag('gm-notes', 'notes');
-                return `fas ${notes ? 'fa-clipboard-check' : 'fas fa-clipboard'}`
-            },
-            onclick: ev => {
-                new GMNote(app.document, { submitOnClose: true, closeOnSubmit: false, submitOnUnfocus: true }).render(true)
-            }
-        });
-    }
+		// If user is not GM - don't do anything
+		if (!game.user.isGM) return;
 
-    static _updateHeaderButton(app, [elem], options) {
-        // Ignore JournalTextPageSheets
-        if(app instanceof JournalTextPageSheet || !(app.document instanceof foundry.abstract.Document)) return;
+		// If app has document
+		if (!(app.document instanceof foundry.abstract.Document)) return;
 
-        // Make sure elem is parent
-        elem = elem.closest('.window-app');
+		buttons.unshift({
+			// If hide label is true, don't show label
+			label: game.i18n.localize("GMNote.label"),
+			tooltip: game.i18n.localize("GMNote.label"),
+			class: "open-gm-note",
+			get icon() {
+				// Get GM Notes
+				const notes = app.document.getFlag("gm-notes", "notes");
+				return `fas ${notes ? "fa-clipboard-check" : "fas fa-clipboard"}`;
+			},
+			onclick: (ev) => {
+				new GMNote(app.document, { submitOnClose: true, closeOnSubmit: false, submitOnUnfocus: true }).render(true);
+			},
+		});
+	}
 
+	static _updateHeaderButton(app, [elem], options) {
+		// Ignore JournalTextPageSheets
+		if (app instanceof JournalTextPageSheet || !(app.document instanceof foundry.abstract.Document)) return;
 
+		// Make sure elem is parent
+		elem = elem.closest(".window-app");
 
-        // Check if user is GM
-        if (!game.user.isGM) return;
+		// Check if user is GM
+		if (!game.user.isGM) return;
 
-        // Check if elem has header button
-        if (!elem?.querySelector('.open-gm-note')) return;
+		// Check if elem has header button
+		if (!elem?.querySelector(".open-gm-note")) return;
 
-        // Get GM Notes Button
-        const gmNotesButton = elem.querySelector('.open-gm-note');
+		// Get GM Notes Button
+		const gmNotesButton = elem.querySelector(".open-gm-note");
 
-        // Get GM Notes
-        const notes = app.document.getFlag('gm-notes', 'notes');
+		// Get GM Notes
+		const notes = app.document.getFlag("gm-notes", "notes");
 
-        // Set color to green if notes exist
-        gmNotesButton.style.color = game.settings.get('gm-notes', 'colorLabel') && notes ? 'var(--palette-success, green)' : '';
-        // Change icon to Check
-        gmNotesButton.innerHTML = `<i class="fas ${notes ? 'fa-clipboard-check' : 'fas fa-clipboard'}"></i> ${GMNote.shouldHideLabel ? '' : game.i18n.localize('GMNote.label')}`;
-    }
-    
-    async _moveToNotes() {
-        if (game.dnd5e && this.object.constructor.name !== 'JournalEntry') {
-            let descPath = '';
-            switch (this.object.constructor.name) {
-                case 'Actor5e': descPath = 'system.details.biography.value'; break;
-                case 'Item5e': descPath = 'system.description.value'; break;
-            }
-            let description = getProperty(this.object, descPath);
-            let notes = getProperty(this.object, 'flags.gm-notes.notes');
+		// Set color to green if notes exist
+		gmNotesButton.style.color = game.settings.get("gm-notes", "colorLabel") && notes ? "var(--palette-success, green)" : "";
+		// Change icon to Check
+		gmNotesButton.innerHTML = `<i class="fas ${notes ? "fa-clipboard-check" : "fas fa-clipboard"}"></i> ${
+			GMNote.shouldHideLabel ? "" : game.i18n.localize("GMNote.label")
+		}`;
+	}
 
-            if (notes === undefined) notes = '';
-            if (description === undefined) description = '';
+	async _moveToNotes() {
+		if (game.dnd5e && this.object.constructor.name !== "JournalEntry") {
+			let descPath = "";
+			switch (this.object.constructor.name) {
+				case "Actor5e":
+					descPath = "system.details.biography.value";
+					break;
+				case "Item5e":
+					descPath = "system.description.value";
+					break;
+			}
+			let description = getProperty(this.object, descPath);
+			let notes = getProperty(this.object, "flags.gm-notes.notes");
 
-            let obj = {};
-            obj[descPath] = '';            
-            await this.object.setFlag('gm-notes', 'notes' ,notes + description);
-            await this.object.update(obj);
-            // No longeer required - the update will re-render
-            // this.render();
-        } else if(this.object.constructor.name === 'JournalEntry') {
+			if (notes === undefined) notes = "";
+			if (description === undefined) description = "";
 
-            let page = this.getCurrentPage();
-            if(!page) { 
-                // I no current page - don't do things
-                return;
-            }
-            
-            let notes = this.object.getFlag('gm-notes', 'notes') ?? '';
-            let description = getProperty(page, 'text.content') ?? '';
-            // Here can just move text
-            let obj = {};
-            obj["text.content"] = '';
-            await this.object.setFlag('gm-notes', 'notes' ,notes + description);
-            await page.update(obj);
-        }
-    }
+			let obj = {};
+			obj[descPath] = "";
+			await this.object.setFlag("gm-notes", "notes", notes + description);
+			await this.object.update(obj);
+			// No longeer required - the update will re-render
+			// this.render();
+		} else if (this.object.constructor.name === "JournalEntry") {
+			let page = this.getCurrentPage();
+			if (!page) {
+				// I no current page - don't do things
+				return;
+			}
 
-    async _moveToDescription() {
-        if (game.dnd5e && this.object.constructor.name !== 'JournalEntry') {
-            let descPath = '';
-            switch (this.object.constructor.name) {
-                case 'Actor5e': descPath = 'system.details.biography.value'; break;
-                case 'Item5e': descPath = 'system.description.value'; break;
-            }
-            let description = getProperty(this.object, descPath);
-            let notes = this.object.getFlag('gm-notes','notes');
+			let notes = this.object.getFlag("gm-notes", "notes") ?? "";
+			let description = getProperty(page, "text.content") ?? "";
+			// Here can just move text
+			let obj = {};
+			obj["text.content"] = "";
+			await this.object.setFlag("gm-notes", "notes", notes + description);
+			await page.update(obj);
+		}
+	}
 
-            if (notes === undefined) notes = '';
-            if (description === undefined) description = '';
+	async _moveToDescription() {
+		if (game.dnd5e && this.object.constructor.name !== "JournalEntry") {
+			let descPath = "";
+			switch (this.object.constructor.name) {
+				case "Actor5e":
+					descPath = "system.details.biography.value";
+					break;
+				case "Item5e":
+					descPath = "system.description.value";
+					break;
+			}
+			let description = getProperty(this.object, descPath);
+			let notes = this.object.getFlag("gm-notes", "notes");
 
-            let obj = {};
-            obj[descPath] = description + notes;
-            await this.object.setFlag('gm-notes','notes','');       
-            await this.object.update(obj);  // this will re-render
-        } else if(this.object.constructor.name === 'JournalEntry') {
-            let page = this.getCurrentPage();
-            if(!page) {
-                return;
-            }
-            let notes = getProperty(this.object, 'flags.gm-notes.notes') ?? '';
-            let description = getProperty(page, 'text.content') ?? '';
+			if (notes === undefined) notes = "";
+			if (description === undefined) description = "";
 
-            let obj = {};            
-            obj["text.content"] = description + notes;            
-            await this.object.setFlag('gm-notes','notes','');
-            await page.update(obj);
-        }
-    }
+			let obj = {};
+			obj[descPath] = description + notes;
+			await this.object.setFlag("gm-notes", "notes", "");
+			await this.object.update(obj); // this will re-render
+		} else if (this.object.constructor.name === "JournalEntry") {
+			let page = this.getCurrentPage();
+			if (!page) {
+				return;
+			}
+			let notes = getProperty(this.object, "flags.gm-notes.notes") ?? "";
+			let description = getProperty(page, "text.content") ?? "";
+
+			let obj = {};
+			obj["text.content"] = description + notes;
+			await this.object.setFlag("gm-notes", "notes", "");
+			await page.update(obj);
+		}
+	}
 }
 
 Hooks.once('init', () => {
@@ -237,6 +247,19 @@ Hooks.once('init', () => {
         type: String,
         default: '0',
     });
+    // Expose API
+    globalThis.gmnote = {GMNote: GMNote };    
+
+    // Register keybinding
+    const hotkey = {
+        name: 'GMNote.hotKeyName',
+        hint: 'GMNote.hotKeyHint',
+        restricted: true,
+        onDown: () => { GMNote.showGMNoteWindow(); },
+        onUp: () => {}
+    };
+
+    game.keybindings.register("gm-notes",'showGMNote',hotkey);
 
 });
 
